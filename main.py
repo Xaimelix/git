108e19261251b687f44ff1c96e389fcab9cb0757
Xamix <Xaimfolk355@yandex.ru>
2022-11-22 18:25:03 +1000

    new commit1

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def main():
     print('And I change it every day')
 
 
+def a():
+    return 1 + 6
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
