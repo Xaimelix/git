ad4fbe698d5c3b42ac399154aab0c8456ed3af62
Xami <Xaimfolk355@yandex.ru>
2022-11-25 19:42:14 +1000

    done

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+from random import randint
+from git_ui import Ui_MainWindow
+
+
+class A(Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        a = ([randint(1, 500)] * 2)
+        print(a)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(QPoint(400, 300), *a)
 
 
 if __name__ == '__main__':
-    main()
-print(2)
+    app = QApplication(sys.argv)
+    ex = A()
+    ex.show()
+    sys.exit(app.exec())
